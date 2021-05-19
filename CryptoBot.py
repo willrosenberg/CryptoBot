45b43177bef486d105bce401202d17b03e79a168
Mowteam <mowteam1@gmail.com>
2021-05-19 14:06:48 -0700

    retrieved data from JSON dictionary

--- CryptoBot.py
+++ CryptoBot.py
@@ -2,5 +2,7 @@ import requests
 import json
 
 response = requests.get("https://api.coinbase.com/v2/exchange-rates")
-#response.status_code
+#response.status_code == 200
 print(json.dumps(response.json(), sort_keys=True, indent=4))
+
+BTC_price = float(response.json()['data']['rates']['BTC'])
